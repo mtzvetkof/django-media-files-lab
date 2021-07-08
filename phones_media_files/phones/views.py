77dec2f330c877e723c8aa016260d6f1ac34ca51
mtsvetkoff <mtzvetkoff@abv.bg>
2021-07-09 00:55:36 +0300

    Static files settings added. Sample main.css added to demonstrate the use of css files

--- phones_media_files/phones/views.py
+++ phones_media_files/phones/views.py
@@ -1,11 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+from phones_media_files.phones.forms import PhoneForm
 from phones_media_files.phones.models import Phone
 
 
 def index(request):
     context = {
         'phones': Phone.objects.all(),
+        'form': PhoneForm()
     }
 
     return render(request, 'index.html', context)
+
+
+def create_phone(request):
+    form = PhoneForm(request.POST)
+    if form.is_valid():
+        form.save()
+        return redirect('index')
