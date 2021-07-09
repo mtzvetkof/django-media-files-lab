dbbd763a6c75746d5f795e7f391a3b015845d549
mtsvetkoff <mtzvetkoff@abv.bg>
2021-07-10 01:26:29 +0300

    1

--- phones_media_files/phones/forms.py
+++ phones_media_files/phones/forms.py
@@ -1,9 +1,24 @@
 from django import forms
 
-from phones_media_files.phones.models import Phone
+from phones_media_files.phones.models import Phone, PhoneImage
 
 
 class PhoneForm(forms.ModelForm):
+    image = forms.ImageField()
+
+    def save(self, commit=True):
+        phone = super().save(commit=commit)
+
+        phone_image = PhoneImage(
+            image = self.cleaned_data['image'],
+            phone = phone,
+            is_selected=True,
+        )
+        if commit:
+            phone_image.save()
+
+
     class Meta:
         model = Phone
         fields = '__all__'
+
